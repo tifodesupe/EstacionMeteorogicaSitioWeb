f071a6b83d4742e9cf902f53b7cb013042801ab9
matías <matiasmongua@gmail.com>
2023-08-26 09:34:15 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const presion = document.getElementById("presionBarometrica")
+
+presion.innerHTML = "Hola mundo"
+const temperatura = document.getElementById("temperaturaAmbiente")
+
+temperatura.innerHTML = "Hola mundo"
+
+const humedad = document.getElementById("humedadRelativa")
+
+humedad.innerHTML = "Hola mundo"
